e7e3aa9b5fa78b30f959ca8c264f04e199738fc7
SICGames <SICGames88@gmail.com>
2025-02-03 12:32:23 -0500

    patched NullException in line 58 of FileSubscriber

--- Loggio/Extensions/File/FileSubscriber.cs
+++ Loggio/Extensions/File/FileSubscriber.cs
@@ -55,10 +55,12 @@ namespace com.HellStormGames.Diagnostics.Logging
                 {
                     var loggioEx = loggioEvent.Exception;
                     StackTrace stackTrace = new StackTrace(loggioEx, true);
-                    StackFrame? frame = stackTrace.GetFrames().Where(f => f.GetFileName() != null).FirstOrDefault();
-
-                    exceptionString = $"\n\t Exception Information: \n\t\t Type: {loggioEx.GetType()}\n\t\t Message: {loggioEx.Message}\n\t\t" +
-                        $" Method Name: {frame?.GetMethod()?.Name}\n\t\t Line: {frame?.GetFileLineNumber()}.\n\t\t Source File: {frame?.GetFileName()}\n";
+                    StackFrame? frame = stackTrace?.GetFrames()?.Where(f => f?.GetFileName() != null)?.FirstOrDefault();
+                    if (frame != null)
+                    {
+                        exceptionString = $"\n\t Exception Information: \n\t\t Type: {loggioEx.GetType()}\n\t\t Message: {loggioEx.Message}\n\t\t" +
+                            $" Method Name: {frame?.GetMethod()?.Name}\n\t\t Line: {frame?.GetFileLineNumber()}.\n\t\t Source File: {frame?.GetFileName()}\n";
+                    }
                 }
 
                 var str = $"[{loggioEvent.DateTimeOffset.ToString(@"yyyy-MM-dd HH:mm:ss")}] " +
